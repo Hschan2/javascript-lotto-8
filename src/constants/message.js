b8ab68d5a75b64c8c476c24ec5f2a6cf2d63638e
Hchan2 <seongchan_@naver.com>
2025-11-01 21:30:49 +0900

    feat: declare the require text list

---
+++ src/constants/message.js
@@ -0,0 +1,19 @@
+export const INPUT_MESSAGE = {
+  LOTTO_BUY_AMOUNT: "구입금액을 입력해 주세요.",
+  USER_LOTTO_NUMBERS: "\n당첨 번호를 입력해 주세요.",
+  USER_BONUS_NUMBER: "\n보너스 번호를 입력해 주세요.",
+};
+
+export const OUTPUT_MESSAGE = {
+  BUY_COUNT: "개를 구매했습니다.",
+  WINNING_STATISTICS: "당첨 통계",
+  DIVIDING_LINE: "---",
+};
+
+export const WINNING_DETAILS = {
+  FIFTH: { message: "3개 일치 (5,000원)" },
+  FOURTH: { message: "4개 일치 (50,000원)" },
+  THIRD: { message: "5개 일치 (1,500,000원)" },
+  SECOND: { message: "5개 일치, 보너스 볼 일치 (30,000,000원)" },
+  FIRST: { message: "6개 일치 (2,000,000,000원)" },
+};
