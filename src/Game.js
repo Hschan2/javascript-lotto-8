bd6cf5c70195cba8ba002d1bcff9635827b3a19f
Hchan2 <seongchan_@naver.com>
2025-11-01 21:38:00 +0900

    feat: add lotto process functions with Lotto Class, User Class

---
+++ src/Game.js
@@ -0,0 +1,71 @@
+
+import { readBuyAmount, readWinningNumbers, readBonusNumber } from "./view/InputHandler.js";
+import { printBuyAmount, printLottos, printResult, printEarningRate, printError } from "./view/OutputHandler.js";
+import User from "./User.js";
+import Lotto from "./Lotto.js";
+import { DELIMITER } from "./constants/constant.js";
+
+class Game {
+  #user;
+  #winningLotto;
+
+  async start() {
+    await this.#readBuyAmount();
+    this.#issueLottos();
+    await this.#readWinningLotto();
+    this.#showResult();
+  }
+
+  async #readBuyAmount() {
+    while (true) {
+      try {
+        const amount = await readBuyAmount();
+        this.#user = new User(amount);
+        return;
+      } catch (error) {
+        printError(error.message);
+      }
+    }
+  }
+
+  #issueLottos() {
+    this.#user.buyLottos();
+    printBuyAmount(this.#user.getLottoCount());
+    printLottos(this.#user.getLottos());
+  }
+
+  async #readWinningLotto() {
+    while (true) {
+      try {
+        const { winningNumbersArr, bonusNumber } = await this.#getAndValidateWinningLottoInput();
+        this.#winningLotto = {
+          numbers: winningNumbersArr,
+          bonus: bonusNumber,
+        };
+        return;
+      } catch (error) {
+        printError(error.message);
+      }
+    }
+  }
+
+  async #getAndValidateWinningLottoInput() {
+    const winningNumbersStr = await readWinningNumbers();
+    const winningNumbersArr = winningNumbersStr.split(DELIMITER).map(Number);
+    new Lotto(winningNumbersArr);
+
+    const bonusNumberStr = await readBonusNumber();
+    Lotto.validateBonusNumber(bonusNumberStr, winningNumbersArr);
+
+    return { winningNumbersArr, bonusNumber: Number(bonusNumberStr) };
+  }
+
+  #showResult() {
+    const result = this.#user.calculateTotalResult(this.#winningLotto);
+    const earningRate = this.#user.calculateEarningRate(result);
+    printResult(result);
+    printEarningRate(earningRate);
+  }
+}
+
+export default Game;
